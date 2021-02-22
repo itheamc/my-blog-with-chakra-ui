b9afab3f77d15f3775aff36320d506b4d4194f21
itheamc <itheamc@gmail.com>
2021-02-22 22:50:10 +0545

    comment out some unused code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import Posts from '../src/components/Posts'
 
 // Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-}
-const theme = extendTheme({ colors })
+// const colors = {
+//   brand: {
+//     900: "#1a365d",
+//     800: "#153e75",
+//     700: "#2a69ac",
+//   },
+// }
+// const theme = extendTheme({ colors })
 
 function App() {
   return (
