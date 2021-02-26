d02f44f7ed1ef7f647ee11cd1eb0daadae2b1765
itheamc <itheamc@gmail.com>
2021-02-26 13:19:24 +0545

    make the title and body more responsive

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -33,7 +33,7 @@ const PostView = (props) => {
                     align = "left"
                     as="h1" 
                     size="lg"
-                    backgroundColor = "red">
+                    noOfLines = {2}>
                     {props.post.title}
                 </Heading>
             </Box>
@@ -43,7 +43,9 @@ const PostView = (props) => {
                     <TagLeftIcon as={TimeIcon} />
                     <TagLabel>Sunday, December 22, 2019</TagLabel>
                 </Tag>
-                <Tag size="sm" variant="subtle">Category</Tag>
+                <Tag size="sm" variant="subtle">
+                    <TagLabel>Category</TagLabel>
+                </Tag>
                 <Tag size="sm" borderRadius="full">
                     <Avatar
                         src="https://bit.ly/sage-adebayo"
@@ -62,8 +64,9 @@ const PostView = (props) => {
             <Box w = "100%" className = "post_content">
                 <Text
                     fontSize="xl"
-                    align = "left">
-                    {props.post.body}....
+                    align = "left"
+                    noOfLines = {3}>
+                    {props.post.body}
                 </Text>
             </Box>
                         
